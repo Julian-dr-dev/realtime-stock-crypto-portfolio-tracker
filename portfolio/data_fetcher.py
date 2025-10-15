6c68ffb79b2b6d347dfe61b13750cb857ed84225
Julian Martinez <julianmartinez@Julians-MacBook-Pro-6.local>
2025-10-15 00:16:28 -0700

    data fetcher.py finished

--- portfolio/data_fetcher.py
+++ portfolio/data_fetcher.py
@@ -2,6 +2,8 @@
 
 import yfinance as yf 
 import requests 
+from datetime import datetime
+
 
 
 class DataFetcher: 
@@ -11,11 +13,11 @@ class DataFetcher:
 
     def get_stock_price(self, ticker: str) -> float:
         try:
-            stock = yf.Ticker
-            price = yf.Ticker(ticker).history(period="1d")["Close"].iloc[-1]
+            stock = yf.Ticker(ticker)
+            price = stock.history(period="1d")["Close"].iloc[-1]
             return price
         except Exception as e:
-            print("Erroer fetching stock price for {ticker}: {e}")
+            print(f"Erroer fetching stock price for {ticker}: {e}")
             return None
         
     def get_stock_history(self, ticker: str, period="1mo") -> dict:
@@ -26,6 +28,19 @@ class DataFetcher:
         except Exception as e:
             print("Error Fetching stock history for {ticker}: {e}")
             return {}
+    def get_crypto_price(self, symbol: str, currency="USD") -> float:
+        try:
+            response: requests.get(
+                self.crypto_api_url,
+                params={"fysm": symbol.upper(), "tsyms": currency.upper()},
+            )
+            response.raise_for_status()
+            data = response.json()
+            return data.get(currency.upper())
+        except Exception as e:
+            print(f"[{datetime.now()}] Error fetching crypto price for {symbol}: {e}")
+            return None
+        
 
 
 
